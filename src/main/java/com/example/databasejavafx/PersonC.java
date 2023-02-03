02d0160809de169f97b8c548972d9ca93eb162aa
Fellograce <trinh.christian31@gmail.com>
2023-02-03 09:06:03 +0100

    View

---
+++ src/main/java/com/example/databasejavafx/PersonC.java
@@ -0,0 +1,59 @@
+package com.example.databasejavafx;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PersonC {
+    @FXML
+    private TextField tfID;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    private TextField tfWohnort;
+
+    @FXML
+    private Button btSubmit;
+
+    @FXML
+    private TableView<?> tvList;
+
+    @FXML
+    private TableColumn<?, ?> tcID;
+
+    @FXML
+    private TableColumn<?, ?> tcName;
+
+    @FXML
+    private TableColumn<?, ?> tcWohnort;
+
+    @FXML
+    private void btSubmitOnAction(ActionEvent event) {}
+
+    public static void show(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(PersonC.class.getResource("insertWindow.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Person");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Platform.exit();
+        }
+    }
+}
